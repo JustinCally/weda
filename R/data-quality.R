54e5f7dee6ec5bdd527cbb1291f98d3a82b46654
Justin Cally <jcally@student.unimelb.edu.au>
2023-03-17 13:55:37 +1100

    fiq dq function colnames

--- R/data-quality.R
+++ R/data-quality.R
@@ -10,47 +10,46 @@ camera_trap_dq <- function(camtrap_records,
                                camtrap_operation,
                                project_information) {
 
-  req_cols <- c('SiteID' ,'SubStation', 'Iteration', 'scientific_name', 'common_name' ,'DateTimeOriginal' ,'Date' ,'Time' ,'delta.time.secs' ,'delta.time.mins' ,'delta.time.hours' ,'delta.time.days' ,'Directory' ,'FileName' ,'n_images' ,'HierarchicalSubject')
-  # this is a vector of column names that are required to be in the camtrap_records dataframe
-
-
   # this is a vector of column names that are required to be in the camtrap_operation dataframe
+  req_cols <- c('SiteID' ,'SubStation', 'Iteration', 'scientific_name', 'common_name' ,'DateTimeOriginal' ,'Date' ,'Time' ,'delta.time.secs' ,'delta.time.mins' ,'delta.time.hours' ,'delta.time.days' ,'Directory' ,'FileName' ,'n_images' ,'HierarchicalSubject')
+
   if(project_information$DistanceSampling) {
 
   # this is a vector of column names that are required to be in the project_information dataframe
     req_cols <- c(req_cols, "metadata_Distance")
   }
   # Create a pointblank object
-  pb_rec <- pointblank::create_agent(
+
+pb_rec <- pointblank::create_agent(
     tbl = camtrap_records,
     actions = pointblank::action_levels(stop_at = 1)) %>%
     pointblank::col_exists(columns = req_cols) %>%
     pointblank::rows_distinct() %>%
-    pointblank::col_is_character(pointblank::vars(SiteID, SubStation, scientific_name, common_name, Time, Directory, FileName)) %>%
-    pointblank::col_is_integer(pointblank::vars(Iteration)) %>%
-    pointblank::col_vals_in_set(pointblank::vars(SiteID), set = camtrap_operation$SiteID) %>%
-    pointblank::col_vals_in_set(pointblank::vars(SubStation), set = camtrap_operation$SubStation) %>%
-    pointblank::col_vals_in_set(pointblank::vars(scientific_name), set = unique(vba_name_conversions$scientific_name)) %>%
-    pointblank::col_vals_in_set(pointblank::vars(common_name), set = unique(vba_name_conversions$common_name)) %>%
+    pointblank::col_is_character(c("SiteID", "SubStation", "scientific_name", "common_name", "Time", "Directory", "FileName")) %>%
+    pointblank::col_is_integer("Iteration") %>%
+    pointblank::col_vals_in_set("SiteID", set = camtrap_operation$SiteID) %>%
+    pointblank::col_vals_in_set("SubStation", set = camtrap_operation$SubStation) %>%
+    pointblank::col_vals_in_set("scientific_name", set = unique(vba_name_conversions$scientific_name)) %>%
+    pointblank::col_vals_in_set("common_name", set = unique(vba_name_conversions$common_name)) %>%
     pointblank::col_vals_not_null(c("SiteID", "scientific_name", "common_name", "Date", "Time", "DateTimeOriginal", "Iteration")) %>%
-    pointblank::col_is_date(pointblank::vars(Date)) %>%
-    pointblank::col_is_posix(pointblank::vars(DateTimeOriginal)) %>%
+    pointblank::col_is_date("Date") %>%
+    pointblank::col_is_posix("DateTimeOriginal") %>%
     pointblank::interrogate()
 
-  pb_op <- pointblank::create_agent(
+pb_op <- pointblank::create_agent(
     tbl = camtrap_operation,
     actions = pointblank::action_levels(stop_at = 1)) %>%
     pointblank::col_exists(columns = c('SiteID', 'SubStation', 'Iteration', 'Latitude', 'Longitude', 'DateDeploy', 'TimeDeploy', 'DateRetrieve', 'TimeRetrieve', 'Problem1_from', 'Problem1_to', 'DateTimeDeploy', 'DateTimeRetrieve', 'CameraHeight', 'CameraID')) %>%
     pointblank::rows_distinct() %>%
-    pointblank::col_is_character(pointblank::vars(SiteID, SubStation, CameraID)) %>%
-    pointblank::col_is_numeric(pointblank::vars(Latitude, Longitude, CameraHeight)) %>%
-    pointblank::col_is_date(pointblank::vars(DateDeploy, DateRetrieve)) %>%
-    pointblank::col_is_integer(pointblank::vars(Iteration)) %>%
-    pointblank::col_is_posix(pointblank::vars(DateTimeDeploy, DateTimeRetrieve, Problem1_from, Problem1_to)) %>%
-    pointblank::col_vals_in_set(pointblank::vars(SiteID), set = camtrap_records$SiteID) %>%
-    pointblank::col_vals_in_set(pointblank::vars(SubStation), set = camtrap_records$SubStation) %>%
-    pointblank::col_vals_between(columns = pointblank::vars(Latitude), left = -60.55, right = -8.47) %>%
-    pointblank::col_vals_between(columns = pointblank::vars(Longitude), left = 93.41, right = 173.34) %>%
+    pointblank::col_is_character(columns = c('SiteID', 'SubStation', 'CameraID')) %>%
+    pointblank::col_is_numeric(columns = c('Latitude', 'Longitude', 'CameraHeight')) %>%
+    pointblank::col_is_date(columns = c('DateDeploy', 'DateRetrieve')) %>%
+    pointblank::col_is_integer(columns = c('Iteration')) %>%
+    pointblank::col_is_posix(columns = c('DateTimeDeploy', 'DateTimeRetrieve', 'Problem1_from', 'Problem1_to')) %>%
+    pointblank::col_vals_in_set(columns = c('SiteID'), set = camtrap_records$SiteID) %>%
+    pointblank::col_vals_in_set(columns = c('SubStation'), set = camtrap_records$SubStation) %>%
+    pointblank::col_vals_between(columns = c('Latitude'), left = -60.55, right = -8.47) %>%
+    pointblank::col_vals_between(columns = c('Longitude'), left = 93.41, right = 173.34) %>%
     pointblank::col_vals_not_null(c('SiteID', 'Latitude', 'Longitude', 'DateDeploy', 'TimeDeploy', 'DateRetrieve', 'TimeRetrieve', 'DateTimeDeploy', 'DateTimeRetrieve', 'CameraHeight', 'CameraID', 'Iteration')) %>%
     pointblank::interrogate()
 
@@ -59,14 +58,13 @@ camera_trap_dq <- function(camtrap_records,
     actions = pointblank::action_levels(stop_at = 1)) %>%
     pointblank::row_count_match(1) %>%
     pointblank::col_vals_not_null(dplyr::everything()) %>%
-    pointblank::col_is_logical(pointblank::vars(DistanceSampling, AllSpeciesTagged)) %>%
-    pointblank::col_vals_in_set(pointblank::vars(TerrestrialArboreal), set = c("Terrestrial", "Arboreal")) %>%
-    pointblank::col_vals_in_set(pointblank::vars(BaitedUnbaited), set = c("Baited", "Unbaited")) %>%
-    pointblank::col_vals_in_set(pointblank::vars(BaitType), set = c("None", "Creamed Honey", "Small Mammal Bait", "Predator Bait")) %>%
+    pointblank::col_is_logical(c("DistanceSampling", "AllSpeciesTagged")) %>%
+    pointblank::col_vals_in_set("TerrestrialArboreal", set = c("Terrestrial", "Arboreal")) %>%
+    pointblank::col_vals_in_set("BaitedUnbaited", set = c("Baited", "Unbaited")) %>%
+    pointblank::col_vals_in_set("BaitType", set = c("None", "Creamed Honey", "Small Mammal Bait", "Predator Bait")) %>%
     pointblank::interrogate()
 
   return(list(camtrap_records = pb_rec,
               camtrap_operation = pb_op,
               project_information = pb_pi))
 }
-
