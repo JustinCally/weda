10bfca0875b1ec97e0eebbda820d929d65075d92
Justin Cally <jcally@student.unimelb.edu.au>
2023-04-12 15:24:25 +1000

    use taxon_id in vba_name_conversions

--- data-raw/vba_name_conversions.R
+++ data-raw/vba_name_conversions.R
@@ -1,4 +1,11 @@
 ## code to prepare `vba_name_conversions` dataset goes here
+library(dplyr)
+## Read in the DAT file
+## Sourced from data.vic
+vba_name_conversions <- sf::st_read("data-raw/VBA_TAXA_LIST") %>%
+  dplyr::select(taxon_id = TAXON_ID,
+                scientific_name = SCI_NAME,
+                common_name = COMM_NAME)
 
 vba_name_conversions <- readRDS("data-raw/vba_name_conversions.rds")
 
