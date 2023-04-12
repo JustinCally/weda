8c58c83c7c3ec2323f3606dfe39f1ffbd8f459b2
Justin Cally <jcally@student.unimelb.edu.au>
2023-04-12 15:29:39 +1000

    document with taxon id

--- data-raw/vba_name_conversions.R
+++ data-raw/vba_name_conversions.R
@@ -5,8 +5,9 @@ library(dplyr)
 vba_name_conversions <- sf::st_read("data-raw/VBA_TAXA_LIST") %>%
   dplyr::select(taxon_id = TAXON_ID,
                 scientific_name = SCI_NAME,
-                common_name = COMM_NAME)
+                common_name = COMM_NAME) %>%
+  distinct()
 
-vba_name_conversions <- readRDS("data-raw/vba_name_conversions.rds")
+# vba_name_conversions <- readRDS("data-raw/vba_name_conversions.rds")
 
 usethis::use_data(vba_name_conversions, overwrite = TRUE)
