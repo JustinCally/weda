89528965ff9b7ccc593e4bf44f30f1181d4a2962
Justin Cally <jcally@student.unimelb.edu.au>
2023-04-26 09:48:15 +1000

    shiny dev

--- R/shiny-upload.R
+++ R/shiny-upload.R
@@ -1,16 +1,66 @@
+capture_cli_messages <- function(fun) {
+  function(..., .quiet = TRUE) {
+
+    output <- list(result = NULL, messages = NULL)
+
+    output$messages <- cli::cli_fmt({
+      output$result <- fun(...)
+    })
+
+    if (!.quiet) cat(output$messages, sep = "\n")
+
+    output
+
+  }
+}
+
+standardise_species_names2 <- capture_cli_messages(weda::standardise_species_names)
+
+
 dataUploadpUI <- function(id,
                          label = "dataUpload") {
+  shinyjs::useShinyjs()
   ns <- shiny::NS(id)
   shiny::tabPanel("Data Upload",
                   sidebarLayout(
                     sidebarPanel = sidebarPanel(
-                      shiny::tags$h4("Step 1"),
+                      shiny::tags$h4("Step 1"), htmlOutput(outputId = ns("step1tick")),
+                      actionButton(inputId = ns("generateCode"),
+                                   label = "Generate camtrapR code",
+                                   icon = icon("code"), width = "100%"),
+                      shiny::tags$h4("Step 2"),
+                      htmlOutput(outputId = ns("step2")),
                       actionButton(inputId = ns("RecordButton"),
                                    label = "Upload Camera Records",
                                    icon = icon("upload"), width = "100%"),
-                      width = 2),
-                    mainPanel = mainPanel(htmlOutput(outputId = ns("step1")),
-                                          width = 10)
+                      shiny::tags$h4("Step 3"),
+                      htmlOutput(outputId = ns("step3")),
+                      actionButton(inputId = ns("OperationButton"),
+                                   label = "Upload Camera Operation",
+                                   icon = icon("upload"), width = "100%"),
+                      shiny::tags$h4("Step 4"),
+                      htmlOutput(outputId = ns("step4")),
+                      actionButton(inputId = ns("ProjectButton"),
+                                   label = "Upload Project Information",
+                                   icon = icon("upload"), width = "100%"),
+                      shiny::tags$h4("Step 5"),
+                      htmlOutput(outputId = ns("step5")),
+                      shinyWidgets::radioGroupButtons(
+                        inputId = ns("nameformat"),
+                        label = "Species Name Format",
+                        choices = c("scientific", "common"),
+                        size = "sm",
+                        status = "primary", width = "45%"),
+                      shiny::textInput(inputId = ns("speciescol"),
+                                       label = "Column name of species",
+                                       value = "Species", width = "45%"),
+                      actionButton(inputId = ns("standardise"),
+                                   label = "Standardise Species Names",
+                                   icon = icon("equals"), width = "100%"),
+                      width = 3),
+                    mainPanel = mainPanel(verbatimTextOutput(outputId = ns("step1")),
+                                          htmlOutput(outputId = ns("standardisecli")),
+                                          width = 9)
                   ))
 
 
@@ -23,22 +73,115 @@ dataUploadServer <- function(id) {
     id,
     function(input, output, session) {
 
-      myModal <- function() {
+      #### Step 1 ####
+      stp1_code <- shiny::eventReactive(input$generateCode, {
+' # For you to upload camera trap data to the database tagged images need to have metadata extracted from them.
+ # To extract metadata from the images use the camtrapR package to generate a table of all records. Below is an
+ # example you can try running. If there are many images this process might take a long time so it is
+ # best to save the resulting table (e.g. raw_camtrap_records) as an rds, excel or csv which you can then upload in step 2.
+
+        raw_camtrap_records <- camtrapR::recordTable(inDir  = YOUR_IMAGE_PATH,
+                               IDfrom = "metadata",
+                               cameraID = "directory",
+                               stationCol = "SiteID",
+                               camerasIndependent = TRUE,
+                               timeZone = Sys.timezone(location = TRUE),
+                               metadataSpeciesTag = "Species",
+                               removeDuplicateRecords = FALSE,
+                               returnFileNamesMissingTags = TRUE)
+
+                    saveRDS(raw_camtrap_records, "raw_camtrap_records.rds")'
+      })
+
+      output$step1 <- shiny::renderText({
+        stp1_code()
+      })
+
+      output$step1tick <- shiny::renderText({
+        req(stp1_code())
+        "&#10003; Step 1 Complete"
+      })
+
+      #### Step 2 ####
+      step2mod <- function() {
         ns <- session$ns
         modalDialog(import_ui(ns("UploadRecords"), from = "file"))
       }
 
-      observeEvent(input$RecordButton, {
-        showModal(myModal())
+      shiny::observeEvent(input$RecordButton, {
+        showModal(step2mod())
       })
-
-
 
       recs <- datamods::import_server("UploadRecords", return_class = "tbl_df")
 
-      output$step1 <- renderText({
+      output$step2 <- renderText({
         req(recs$data())
-        "&#10003; Step 1 Complete"
+        "&#10003; Step 2 Complete"
       })
+
+      #### Step 3 ####
+
+      step3mod <- function() {
+        ns <- session$ns
+        modalDialog(import_ui(ns("UploadOperation"), from = "file"))
+      }
+
+      shiny::observeEvent(input$OperationButton, {
+        showModal(step3mod())
+      })
+
+      opers <- datamods::import_server("UploadOperation", return_class = "tbl_df")
+
+      output$step3 <- renderText({
+        req(opers$data())
+        "&#10003; Step 3 Complete"
+      })
+
+      #### Step 4 ####
+
+      step4mod <- function() {
+        ns <- session$ns
+        modalDialog(import_ui(ns("UploadProject"), from = "file"))
+      }
+
+      shiny::observeEvent(input$ProjectButton, {
+        showModal(step4mod())
+      })
+
+      projs <- datamods::import_server("UploadProject", return_class = "tbl_df")
+
+      output$step4 <- renderText({
+        req(projs$data())
+        "&#10003; Step 4 Complete"
+      })
+
+      #### Step 5 ####
+
+      st_data <- shiny::eventReactive(input$standardise, {
+
+        cat("In standardise 1")
+        # req(input$nameformat)
+        # req(input$speciescol)
+        # req(recs$data())
+        cat("In standardise 2")
+
+        standardise_species_names2(
+            recordTable = recs$data(),
+            format = input$nameformat,
+            speciesCol = input$speciescol,
+            return_data = TRUE)
+
+      })
+
+      output$standardisecli <- renderText({
+        req(st_data())
+
+        op <- st_data()
+        paste("Camera Trap Record Names have been standardised according to the VBA taxonomy.
+              Check the conversions below, if there are species without conversions then you need to ammend your data
+              by either filtering out records of non-taxa (e.g. 'person') or changing the names in the data to match VBA conventions. <br>",
+        paste(cli::ansi_html(op[["messages"]]), collapse = "<br>"))
+      })
+
 })
 }
--- data-raw/schemas.R
+++ data-raw/schemas.R
@@ -6,6 +6,11 @@ ksource <- function(x, ...) {
 ksource("vignettes/camtrap-upload.Rmd")
 
 camtrap_record_schema <- pointblank::col_schema(raw_camtrap_records_fixed)
+camera_trap_records <- raw_camtrap_records %>%
+  mutate(metadata_Multiples = coalesce(as.integer(metadata_Multiples), 1L))
+
+
+saveRDS(raw_camtrap_records, "inst/app/raw_camtrap_records.rds")
 # usethis::use_data(camtrap_record_schema, overwrite = TRUE)
 
 camtrap_operation_schema <- pointblank::col_schema(operationdata %>%
@@ -14,3 +19,18 @@ camtrap_operation_schema <- pointblank::col_schema(operationdata %>%
 
 camtrap_project_schema <- pointblank::col_schema(projectdata)
 # usethis::use_data(camtrap_project_schema, overwrite = TRUE)
+
+library(openxlsx)
+options("openxlsx.datetimeFormat" = "yyyy-mm-dd hh:mm:ss")
+dd_filtered <- weda::data_dictionary %>%
+  filter(table_type == "raw" & column_name %in% c(colnames(camera_trap_records),
+                                                  colnames(operationdata),
+                                                  colnames(projectdata)))
+
+list_of_datasets <- list("camera_trap_records" = camera_trap_records,
+                         "camera_trap_operation" = operationdata,
+                         "camera_trap_project" = projectdata,
+                         "data_dictionary" = dd_filtered)
+
+write.xlsx(list_of_datasets, file = "inst/app/camera_trap_templates.xlsx", overwrite = TRUE)
+
