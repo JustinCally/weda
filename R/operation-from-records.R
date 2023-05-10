c69feeadbd1f6686115e130a6c511fc21821c9de
Justin Cally <jcally@student.unimelb.edu.au>
2023-05-11 09:53:23 +1000

    Update operation generate table. Closes #18

--- R/operation-from-records.R
+++ R/operation-from-records.R
@@ -1,23 +1,40 @@
 #' Operation from records
 #'
 #' @param records camera trap records from camtrapR  with Species, SiteID and SubStation
-#' @param start_tag Species tag for camera deploy (default is "AAAA")
-#' @param end_tag Species tag for camera pickup (default is "AAAA")
+#' @param get_cam_model logical flag (default is TRUE) of whether you want to get cam model and serial number (drive with photos needs to be attached)
 #'
 #' @return data.frame
 #' @export
-camtrap_operation_from_records <- function(records,
-                                           start_tag = "AAAA",
-                                           end_tag = "ZZZ") {
+#' @example
+#' raw_camtrap_records <- camtrapR::recordTable(inDir  = system.file("dummydata/images", package = "weda"),
+#'                                              IDfrom = "metadata",
+#'                                              cameraID = "directory",
+#'                                              stationCol = "SiteID",
+#'                                              camerasIndependent = TRUE,
+#'                                              timeZone = Sys.timezone(location = TRUE),
+#'                                              metadataSpeciesTag = "Species",
+#'                                              removeDuplicateRecords = FALSE,
+#'                                              returnFileNamesMissingTags = TRUE) %>%
+#'                     rename(SubStation = Camera) %>%
+#'                     mutate(SubStation = case_when(SiteID == SubStation ~ NA_character_,
+#'                                                  TRUE ~ SubStation),
+#'                                                       Iteration = 1L)
+#'
+#'  op_table <- camtrap_operation_from_records(raw_camtrap_records)
+#'
+camtrap_operation_from_records <- function(records, get_cam_model = TRUE) {
 
   setup_packdown <- records %>%
-    dplyr::filter(Species %in% c("AAAA", "ZZZ")) %>%
     dplyr::group_by(SiteID, SubStation, Iteration) %>%
-    dplyr::summarise(DateTimeDeploy = max(DateTimeOriginal[Species == "AAAA"]),
-                     DateTimeRetrieve = min(DateTimeOriginal[Species == "ZZZ"])) %>%
+    dplyr::summarise(DateTimeDeploy = min(DateTimeOriginal),
+                     DateTimeRetrieve = max(DateTimeOriginal)) %>%
     dplyr::ungroup() %>%
     dplyr::mutate(TimeDeploy = format(DateTimeDeploy, format="%H:%M:%S"),
                   TimeRetrieve = format(DateTimeRetrieve, format="%H:%M:%S"))
+
+  if(!get_cam_model) {
+    return(setup_packdown)
+  } else{
 
   files_to_extract <- records %>%
     dplyr::group_by(SiteID, SubStation, Iteration) %>%
@@ -29,10 +46,10 @@ camtrap_operation_from_records <- function(records,
 
   for(i in 1:nrow(files_to_extract)) {
     tags[[i]] <- camtrapR::exifTagNames(fileName = files_to_extract$FileFullPath[i]) %>%
-      dplyr::filter(tag_name %in% c("Model", "SerialNumber")) %>%
+      dplyr::filter(tag_name %in% c("UserLabel", "SerialNumber")) %>%
       dplyr::select(-tag_group) %>%
       tidyr::pivot_wider(names_from = tag_name, values_from = value) %>%
-      dplyr::rename(CameraModel = Model, CameraID = SerialNumber)
+      dplyr::rename(CameraModel = UserLabel, CameraID = SerialNumber)
   }
 
   tags_bound <- dplyr::bind_rows(tags)
@@ -40,23 +57,5 @@ camtrap_operation_from_records <- function(records,
   return_data <- dplyr::bind_cols(setup_packdown, tags_bound)
 
   return(return_data)
+  }
 }
-#
-# raw_camtrap_records <- recordTable(inDir  = system.file("dummydata/images", package = "weda"),
-#                                    IDfrom = "metadata",
-#                                    cameraID = "directory",
-#                                    stationCol = "SiteID",
-#                                    camerasIndependent = TRUE,
-#                                    timeZone = Sys.timezone(location = TRUE),
-#                                    metadataSpeciesTag = "Species",
-#                                    removeDuplicateRecords = FALSE,
-#                                    returnFileNamesMissingTags = TRUE) %>%
-#   rename(SubStation = Camera) %>%
-#   mutate(SubStation = case_when(SiteID == SubStation ~ NA_character_,
-#                                 TRUE ~ SubStation),
-#          Iteration = 1L) %>%
-#   dplyr::filter(SiteID %in% c("56505", "9941")) %>%
-#   mutate(Species = c("AAAA", "ZZZ", "AAAA", "ZZZ"),
-#          SubStation = NA_character_)
-#
-# camtrap_operation_from_records(raw_camtrap_records)
