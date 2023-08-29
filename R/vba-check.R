12f35460303635585d54db8cc27a21b9c50c4dba
Justin Cally <36715313+JustinCally@users.noreply.github.com>
2023-08-29 12:59:09 +1000

    Data download vignette

--- R/vba-check.R
+++ R/vba-check.R
@@ -41,7 +41,7 @@ standardise_species_names <- function(recordTable,
   if(length(which_not_in > 0)) {
     warning("No match found for ",
             paste(which_not_in, collapse = ", "),
-            ". Please provide names within the VBA taxa list")
+            ". Please provide names within the VBA taxa list\n")
   }
 
 
@@ -52,6 +52,7 @@ standardise_species_names <- function(recordTable,
     dplyr::ungroup() %>%
     dplyr::filter((rows > 1 & stringr::str_detect(!!rlang::sym(other_name), "fam.", negate = T)) | rows == 1) %>%
     dplyr::group_by(!!rlang::sym(var_name)) %>%
+    dplyr::slice(1) %>%
     dplyr::summarise(dplyr::across(!!rlang::sym(other_name), .fns = ~ paste(.x, collapse = "/")),
                      n = dplyr::n()) %>%
     dplyr::ungroup() %>%
