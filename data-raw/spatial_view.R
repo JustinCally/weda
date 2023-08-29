b3ddef8b15fb0aaf5a9fd590c428f4dee9e484ac
Justin Cally <36715313+JustinCally@users.noreply.github.com>
2023-08-29 15:45:09 +1000

    add news, update version

--- data-raw/spatial_view.R
+++ data-raw/spatial_view.R
@@ -6,7 +6,8 @@ con <- weda::weda_connect(password = keyring::key_get(service = "ari-dev-weda-ps
                                                       username = "psql_user"))
 
 geom_table <- tbl(con, in_schema("camtrap", "curated_camtrap_operation")) %>%
-  mutate(geometry = sql('ST_SetSRID(ST_MakePoint("Longitude", "Latitude"), 4283)'))
+  mutate(geometry = sql('ST_SetSRID(ST_MakePoint("Longitude", "Latitude"), 4283)')) %>%
+  select(camtrap_operation_database_ID, everything())
 
 DBI::dbExecute(conn = con, paste(DBI::SQL("CREATE VIEW camtrap.spatial_camtrap_operation AS"), dbplyr::remote_query(geom_table)))
 
